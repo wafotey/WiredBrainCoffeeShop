bd76dcecdb5f2647d1539462258a5503191a145f
wafotey <wafotey@outlook.com>
2021-05-14 05:20:19 +0000

    Added Project to Solution

---
+++ WiredBrainCoffeeShop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiredBrainCoffeeShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to WiredBrain Coffee Shop. Your number one Coffee Shop.");
+        }
+    }
+}
